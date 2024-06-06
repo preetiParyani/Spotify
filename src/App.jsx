b71ab1545239b2e6da02c269cb46a28be322523f
Preeti Paryani <preetiparyani29@gmail.com>
2024-06-06 13:43:25 +0530

    PAta nhi kya horaha hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
+import Login from "./components/Login";
 
 function App() {
-  
-
   return (
     <>
-     <h1>App</h1>
+      <h1>App</h1>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
