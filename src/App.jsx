bed4d5d0c13bed3b5eb1a5ca1363e331b9e1f1d9
Preeti Paryani <preetiparyani29@gmail.com>
2024-06-10 19:56:59 +0530

    All this is very overwhelming

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 
 import "./App.css";
-import Login from "./components/Login";
+import Login from "./components/login";
 
 function App() {
   return (
     <>
-      <h1>App</h1>
       <Login />
     </>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
-import './login.css'
+import React from "react";
+import "./logins.css";
+
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="login">
+      <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" />
+      <a>LOGIN WITH SPOTIFY</a>
+    </div>
+  );
 }
